52dcb448c2d358470c7ae6aa49dd1efff6622921
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-06-12 00:00:24 +0530

    Deploy website - based on 8fc03c0836ec02d0d2f8d7d65be56a9c274ed1eb

--- README.md
+++
@@ -1,2 +0,0 @@
-# bolg
-The occassional ramblings of our agitated developers, manifested in the form of bolgs.
---
+++ assets/js/814f3328.84c1279d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkclassic=self.webpackChunkclassic||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/welcome","unlisted":false},{"title":"Long Blog Post","permalink":"/long-blog-post","unlisted":false},{"title":"First Blog Post","permalink":"/first-blog-post","unlisted":false}]}')}}]);
